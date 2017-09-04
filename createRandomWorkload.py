da1b55f88d25756ce0fa45bde35943965beba974
root <root@acme31.ciemat.es>
2017-09-04 10:53:57 +0200

    pickle to recover old traces

--- createRandomWorkload.py
+++ createRandomWorkload.py
@@ -1,12 +1,3 @@
-#INPUT PARAMETERS:
-#$1: fraction of cluster employed
-#$2: cluster size
-#$3: min job size
-#$4: max job size
-#$5: min job length, in seconds
-#$6: max job length, in seconds
-#$7: desired experiment length in seconds
-
 '''
 Created on 31 aug. 2017
 
@@ -14,6 +5,33 @@ Created on 31 aug. 2017
 
 License: GPL
 '''
+
+'''
+FUNCTIONALITY
+----
+This script creates a Slurm workload for a particular cluster.
+
+This workload (trace) is exported into a tmp file for human read and to a pickle
+ one, to it can be replayed if desired.
+
+It can be executed in two ways:
+- if several input arguments are used, the load is created and executed
+- if only an input argument is used, it is considered to be a pickle file
+from a past execution. It is loaded and replayed
+
+INPUT PARAMETERS:
+$1: fraction of cluster employed
+$2: cluster size
+$3: min job size
+$4: max job size
+$5: min job length, in seconds
+$6: max job length, in seconds
+$7: desired experiment length in seconds
+
+OR
+$1: name of the trace file to replay
+'''
+
 import sys
 from random import randint
 from time import sleep
@@ -21,6 +39,7 @@ import tempfile
 import shutil
 import os
 from numpy.random import normal
+import pickle
 
 class Job:
     jobId = -1
@@ -60,45 +79,81 @@ class Job:
 
 if __name__ == '__main__':
 
-    fractionOfClusterEmployed = float(sys.argv[1])
-    clusterSize = int(sys.argv[2])
-    minJobSize = int(sys.argv[3])
-    maxJobSize = int(sys.argv[4])
-    minJobLength = int(sys.argv[5])
-    maxJobLength = int(sys.argv[6])
-    experimentLength = int(sys.argv[7])
-
-
-    totalResourceAvailability = clusterSize * experimentLength * fractionOfClusterEmployed
 
     jobList = []
-    jobCounter = 0
-    while (totalResourceAvailability > 0):
-        ##gausian distribution of jobs: most of them are small. This is truncated to 1 so it is not perfect, but it's good enough
-        ##1.1 is to allow some values over 1, so jobLength can be maxJobLength sometimes.
-        gaussian =2
-        while gaussian >1.1:
-            gaussian = abs(normal())
-        print gaussian
-        jobSize =max (1,  gaussian * float(maxJobSize))
-        jobLength = randint (minJobLength, maxJobLength)
-        startOffset = randint (0, experimentLength - jobLength)
+    experimentLength = 0
 
-        #check if the job is too big for the remaining resource availability, and trim it if so
-        if jobSize * jobLength > totalResourceAvailability:
-            jobSize = totalResourceAvailability / jobLength
-            totalResourceAvailability = 0  #to avoid problems with integer round and make sure that this is the last job
+    if (len(sys.argv) ==2):
+        pickleFileName = sys.argv[1]
+        pickleFile = open (pickleFileName, 'r')
+        jobList = pickle.load(pickleFile)
+        pickleFile.close()
+        print ("job trace imported from " + pickleFileName)
+
+        #calculate experiment end
+        #number 10 is just in case, to have a small margin
+        experimentLength = 10 + reduce((lambda x, y: max(x,y)), map(lambda x: x.startOffset, jobList))
+        print experimentLength
 
 
-        newJob = Job(jobCounter, jobSize, jobLength, startOffset)
-        jobList.append(newJob)
-        jobCounter +=1
-        totalResourceAvailability -= (jobSize * jobLength)
-        print ("Created job: " + str(newJob))
-        print ("remaining tresources = " + str(totalResourceAvailability))
 
-    jobList.sort(key=lambda job: 0 - job.startOffset)
-    #jobList.sort(key=lambda job: job.jobSize)
+    elif (len(sys.argv) == 8):
+
+        fractionOfClusterEmployed = float(sys.argv[1])
+        clusterSize = int(sys.argv[2])
+        minJobSize = int(sys.argv[3])
+        maxJobSize = int(sys.argv[4])
+        minJobLength = int(sys.argv[5])
+        maxJobLength = int(sys.argv[6])
+        experimentLength = int(sys.argv[7])
+
+
+        totalResourceAvailability = clusterSize * experimentLength * fractionOfClusterEmployed
+
+        jobList = []
+        jobCounter = 0
+        while (totalResourceAvailability > 0):
+            ##gausian distribution of jobs: most of them are small. This is truncated to 1 so it is not perfect, but it's good enough
+            ##1.1 is to allow some values over 1, so jobLength can be maxJobLength sometimes.
+            gaussian =2
+            while gaussian >1.1:
+                gaussian = abs(normal())
+            print gaussian
+            jobSize = int(max (1,  gaussian * float(maxJobSize)))
+            jobLength = randint (minJobLength, maxJobLength)
+            startOffset = randint (0, experimentLength - jobLength)
+
+            #check if the job is too big for the remaining resource availability, and trim it if so
+            if jobSize * jobLength > totalResourceAvailability:
+                jobSize = int(totalResourceAvailability / jobLength)
+                totalResourceAvailability = 0  #to avoid problems with integer round and make sure that this is the last job
+
+
+            newJob = Job(jobCounter, jobSize, jobLength, startOffset)
+            jobList.append(newJob)
+            jobCounter +=1
+            totalResourceAvailability -= (jobSize * jobLength)
+            print ("Created job: " + str(newJob))
+            print ("remaining tresources = " + str(totalResourceAvailability))
+
+        jobList.sort(key=lambda job: 0 - job.startOffset)
+        exportJobListName = tempfile.mkstemp()[1]
+
+        exportJobList = open (exportJobListName, 'w')
+
+        for job in jobList:
+            exportJobList.write(str(job) +"\n")
+        exportJobList.close()
+        print ("job list exported to " + exportJobListName)
+
+        pickleFileName = tempfile.mkstemp()[1]
+        pickleFile = open (pickleFileName, 'w')
+        pickle.dump(jobList, pickleFile)
+        pickleFile.close()
+        print ("job trace exported to " + pickleFileName)
+        exit
+
+
     print ("SORTED LIST OF JOBS")
     for job in jobList:
         print (job)
